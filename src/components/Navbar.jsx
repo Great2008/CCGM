71e205b3b3a11c6e61f56fc612ce5f75891eae24
Great2008 <aakorokaragreatname@gmail.com>
2026-02-28 00:33:04 +0100

    update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import { useAuth } from '../contexts/AuthContext'
 import supabase from '../lib/supabase'
 
 const BELL_SEEN_KEY = 'ccg-notif-last-seen'
-import NotificationCenter from './NotificationCenter'
 
 const NAV_LINKS = [
   { to:'/',         label:'Home' },
