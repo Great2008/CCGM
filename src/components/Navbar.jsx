32b6d3ef52e0fb4f8529ad7457edfde5a0edc5eb
Great2008 <aakorokaragreatname@gmail.com>
2026-02-28 11:27:33 +0100

    update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import { useAuth } from '../contexts/AuthContext'
 import supabase from '../lib/supabase'
 
 const BELL_SEEN_KEY = 'ccg-notif-last-seen'
-import NotificationCenter from './NotificationCenter'
 
 const NAV_LINKS = [
   { to:'/',         label:'Home' },
@@ -145,8 +144,7 @@ export default function Navbar() {
               )}
             </div>
 
-            {/* Notification Bell */}
-            <NotificationCenter user={user} />
+          
 
             {/* Notifications bell */}
             <Link to="/notifications"
