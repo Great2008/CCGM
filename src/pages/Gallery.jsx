841a00508551378f6dc648bfddb8e19157b5d61d
Great2008 <aakorokaragreatname@gmail.com>
2026-03-04 10:05:13 +0100

    update Gallery.jsx

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -171,7 +171,6 @@ export default function Gallery() {
       )}
 
       <style>{`@keyframes pulse{0%,100%{opacity:1}50%{opacity:0.4}}`}</style>
-    </>
-    </PullToRefresh>
+    </></PullToRefresh>
   )
 }
